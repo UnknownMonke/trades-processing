2a1547f17adb19bcc666ef57dd3a81fc308f6111
UnknownMonke <cdeg09@gmail.com>
2025-11-18 20:21:52 +0100

    improve schema with nested records, update commands

--- datagen/commands.sh
+++ datagen/commands.sh
@@ -4,14 +4,19 @@ docker-compose up -d --build
 # ------- Using cURL from Windows Powershell ------- #
 Remove-Item alias:curl
 
+
 # ------- Creates datagen Connector ------- #
-curl -X POST http://localhost:8083/connectors -H 'Content-Type: application/json' --data @datagen/connector.config
+curl -X POST http://localhost:8083/connectors -H 'Content-Type: application/json' --data @datagen/connector.json
+
+# ------- Connector lifecycle ------- #
+curl -X PUT http://localhost:8083/connectors/datagen-trades/pause
+curl -X PUT http://localhost:8083/connectors/datagen-trades/resume
+curl -X DELETE http://localhost:8083/connectors/datagen-trades
+
 
 # ------- Consumes messages from trades topic ------- #
-docker-compose exec kafka-cluster /opt/kafka/bin/kafka-console-consumer.sh --topic trades --bootstrap-server kafka-cluster:29092 --property print.key=true --from-beginning
+docker-compose exec kafka-cluster /opt/kafka/bin/kafka-console-consumer.sh --topic trades --bootstrap-server kafka-cluster:29092 --from-beginning
 
-# ------- Lists topics ------- #
+# ------- Topics management ------- #
 docker-compose exec kafka-cluster /opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka-cluster:9092 --list
-
-# ------- Deletes datagen Connector ------- #
-curl -X DELETE 'http://localhost:8083/connectors/datagen-trades'
+docker-compose exec kafka-cluster /opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka-cluster:9092 --topic trades --delete
